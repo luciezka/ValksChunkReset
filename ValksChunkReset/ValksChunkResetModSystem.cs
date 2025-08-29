67ebce7a6c79ce0f120d53cfa9ff2b200cc29d26
Luciezka <83407389+luciezka@users.noreply.github.com>
2025-08-29 09:10:03 +0200

    So yeah i coded this....

---
+++ ValksChunkReset/ValksChunkResetModSystem.cs
@@ -0,0 +1,70 @@
+﻿using ValksFireStarter.assets.ModConfig;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+
+namespace ValksFireStarter;
+
+public class ValksChunkResetModSystem : ModSystem
+{ 
+    ModConfig fromDisk;
+    string cfgFileName = "ValksChunkReset.json";
+    
+    public override void StartPre(ICoreAPI api)
+    {
+        try
+        {
+            if ((fromDisk = api.LoadModConfig<ModConfig>(cfgFileName)) == null)
+            { api.StoreModConfig(ModConfig.Loaded, cfgFileName); }
+            else
+            { ModConfig.Loaded = fromDisk; }
+        }
+        catch
+        { api.StoreModConfig(ModConfig.Loaded, cfgFileName); }
+        base.StartPre(api);
+    }
+    
+    
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        base.StartServerSide(api);
+            api.Event.ServerRunPhase(EnumServerRunPhase.WorldReady, () =>
+                {
+            if (ModConfig.Loaded.activateReset && resetArchive(api.World.Calendar))
+            {
+                api.Logger.Event("ValksChunkReset: Starting regeneration");
+                int x = ModConfig.Loaded.xCoordinates;
+                int y = ModConfig.Loaded.yCoordiantes;
+
+                int radius =  ModConfig.Loaded.radius;
+                for (int dx = -radius; dx <= radius; dx++)
+                {
+                    for (int dy = -radius; dy <= radius; dy++)
+                    {
+                        api.WorldManager.DeleteChunkColumn(x + dx, y + dy); 
+                    }
+                }
+                api.Logger.Event("ValksChunkReset: Selected Chunks were regenerated");
+                ModConfig.Loaded.lastResetOnDay = (int)api.World.Calendar.ElapsedDays;
+                api.StoreModConfig(ModConfig.Loaded,cfgFileName);
+                
+            }
+        });
+    }
+    
+    public bool resetArchive(IGameCalendar calendar)
+    {
+        if (calendar.ElapsedDays > ModConfig.Loaded.lastResetOnDay +
+            ModConfig.Loaded.resetStructureEveryXDays)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+}
---
+++ ValksChunkReset/assets/valksfirestarter/ModConfig/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace ValksFireStarter.assets.ModConfig;
+
+public class ModConfig
+{
+    public static ModConfig Loaded { get; set; } = new ModConfig();
+    public int xCoordinates  { get; set; } = 0;
+    public int yCoordiantes  { get; set; } = 0;
+    public int radius  { get; set; } = 0;
+    public int resetStructureEveryXDays  = 1;
+    public bool activateReset = false;
+    public int lastResetOnDay;
+        
+}
